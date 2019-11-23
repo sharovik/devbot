ddfc99bbc48f71a70e7c9f7dfef7d1261e795f30
Pavel Simzicov <pavel@tourradar.com>
2019-11-23 15:20:53 +0100

    Covered most of the methods of slack client

--- internal/client/slack_test.go
+++ internal/client/slack_test.go
@@ -4,7 +4,10 @@ import (
 	mockhttp "github.com/karupanerura/go-mock-http-response"
 	"github.com/sharovik/devbot/internal/client"
 	"github.com/sharovik/devbot/internal/container"
+	"github.com/sharovik/devbot/internal/dto"
+	"github.com/sharovik/devbot/test"
 	"github.com/stretchr/testify/assert"
+	"net/http"
 	"testing"
 )
 
@@ -110,4 +113,96 @@ func TestSlackClient_Get(t *testing.T) {
 		assert.NoError(t, err, "There should be no error in response")
 		assert.Equal(t, 200, statusCode, "Status code should be equal 200")
 	})
+}
+
+func TestSlackClient_GetConversationsList_Bad(t *testing.T) {
+	badStatusCases := map[int]string{
+		http.StatusBadGateway:          "Bad gateway",
+		http.StatusInternalServerError: "Internal error",
+		http.StatusNotFound:            "Not found",
+		http.StatusForbidden:           "Forbidden",
+		http.StatusBadRequest:          "Bad request",
+	}
+
+	for status, errorType := range badStatusCases {
+		MockSlackResponse(status, map[string]string{}, []byte(`{"ok": false, "error": "`+errorType+`"}`))
+
+		response, statusCode, err := slackClient.GetConversationsList()
+		assert.Error(t, err)
+		assert.Empty(t, response)
+		assert.Equal(t, status, statusCode)
+	}
+}
+
+func TestSlackClient_GetConversationsList_Ok(t *testing.T) {
+	MockSlackResponse(http.StatusOK, map[string]string{}, test.FileToBytes(t, "../../test/testdata/slack/conversations.list.ok.json"))
+
+	response, statusCode, err := slackClient.GetConversationsList()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, response)
+	assert.NotEmpty(t, response.Channels)
+	assert.Empty(t, response.Error)
+	assert.Equal(t, true, response.Ok)
+	assert.Equal(t, http.StatusOK, statusCode)
+}
+
+func TestSlackClient_GetUsersList_Bad(t *testing.T) {
+	badStatusCases := map[int]string{
+		http.StatusBadGateway:          "Bad gateway",
+		http.StatusInternalServerError: "Internal error",
+		http.StatusNotFound:            "Not found",
+		http.StatusForbidden:           "Forbidden",
+		http.StatusBadRequest:          "Bad request",
+	}
+
+	for status, errorType := range badStatusCases {
+		MockSlackResponse(status, map[string]string{}, []byte(`{"ok": false, "error": "`+errorType+`"}`))
+
+		response, statusCode, err := slackClient.GetUsersList()
+		assert.Error(t, err)
+		assert.Empty(t, response)
+		assert.Equal(t, status, statusCode)
+	}
+}
+
+func TestSlackClient_GetUsersList_Ok(t *testing.T) {
+	MockSlackResponse(http.StatusOK, map[string]string{}, test.FileToBytes(t, "../../test/testdata/slack/users.list.ok.json"))
+
+	response, statusCode, err := slackClient.GetUsersList()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, response)
+	assert.NotEmpty(t, response.Members)
+	assert.Empty(t, response.Error)
+	assert.Equal(t, true, response.Ok)
+	assert.Equal(t, http.StatusOK, statusCode)
+}
+
+func TestSlackClient_SendMessage_Bad(t *testing.T) {
+	badStatusCases := map[int]string{
+		http.StatusBadGateway:          "Bad gateway",
+		http.StatusInternalServerError: "Internal error",
+		http.StatusNotFound:            "Not found",
+		http.StatusForbidden:           "Forbidden",
+		http.StatusBadRequest:          "Bad request",
+	}
+
+	for status, errorType := range badStatusCases {
+		MockSlackResponse(status, map[string]string{}, []byte(`{"ok": false, "error": "`+errorType+`"}`))
+
+		response, statusCode, err := slackClient.SendMessage(dto.SlackRequestChatPostMessage{})
+		assert.Error(t, err)
+		assert.Empty(t, response)
+		assert.Equal(t, status, statusCode)
+	}
+}
+
+func TestSlackClient_SendMessage_Ok(t *testing.T) {
+	MockSlackResponse(http.StatusOK, map[string]string{}, test.FileToBytes(t, "../../test/testdata/slack/users.list.ok.json"))
+
+	response, statusCode, err := slackClient.SendMessage(dto.SlackRequestChatPostMessage{})
+	assert.NoError(t, err)
+	assert.NotEmpty(t, response)
+	assert.Empty(t, response.Error)
+	assert.Equal(t, true, response.Ok)
+	assert.Equal(t, http.StatusOK, statusCode)
 }
